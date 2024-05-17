d48486b5c62f576f7bc7ea8978d0044b93812244
Rensvdk20 <rensvdk@gmail.com>
2024-05-17 15:18:29 +0200

    Added conversation score

---
+++ DELA_game/Assets/Scripts/Score/ScoreManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public Dictionary<string, int> npcScoreList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //fill dictioanry
+        npcScoreList = new Dictionary<string, int>(
+            new KeyValuePair<string, int>[]
+            {
+                new KeyValuePair<string, int>("NPCLake", 0),
+                new KeyValuePair<string, int>("NPCWaterTower", 0),
+                new KeyValuePair<string, int>("NPCFootball", 0),
+                new KeyValuePair<string, int>("NPCForrestHut", 0),
+                new KeyValuePair<string, int>("NPCMines", 0)
+            }
+        );
+    }
+
+    public void AddScore(string npcName)
+    {
+        if (npcScoreList.ContainsKey(npcName))
+            npcScoreList[npcName]++;
+            
+        Debug.Log(npcName + " score: " + npcScoreList[npcName]);
+    }
+}
