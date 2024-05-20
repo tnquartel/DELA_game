ee4cc813d33f7172916d8a870fa431e59fc8bc41
tnquartel <thomasquartel@outlook.com>
2024-05-20 09:30:41 +0200

    created fading tutorial texts

--- DELA_game/Assets/UIScripts/UIManager.cs
+++ DELA_game/Assets/UIScripts/UIManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
@@ -11,10 +12,34 @@ public class UIManager : MonoBehaviour
     public GameObject endPanel;
     public GameObject collectibleCanvas;
     public GameObject regionText;
-    public GameObject instructionTextMove;
+    public TextMeshProUGUI instructionTextMove;
+    public TextMeshProUGUI instructionTextJump;
     public Text playerNameText;
 
     public InputField playerNameInput;
+    public GameObject player;
+
+    private bool isFading = false;
+    private bool shouldMove;
+    private bool shouldJump;
+
+    private void Update()
+    {
+        if (!isFading)
+        {
+            Debug.Log(shouldMove);
+            if (shouldMove && player.transform.position.x > 1 || shouldMove && player.transform.position.x < -1 || shouldMove && player.transform.position.z > -5 || shouldMove && player.transform.position.z < -7)
+            {
+                shouldMove = false;
+                StartCoroutine(FadeOutMoveText());
+            }
+            if (shouldJump && Input.GetKeyDown(KeyCode.Space))
+            {
+                shouldJump = false;
+                StartCoroutine(FadeOutJumpText());
+            }
+        }
+    }
 
     public void HideAllPanels()
     {
@@ -24,7 +49,8 @@ public class UIManager : MonoBehaviour
         endPanel.SetActive(false);
         collectibleCanvas.SetActive(false);
         regionText.SetActive(false);
-        instructionTextMove.SetActive(false);
+        instructionTextMove.gameObject.SetActive(false);
+        instructionTextJump.gameObject.SetActive(false);
     }
 
     public void ShowInGameUI()
@@ -32,8 +58,70 @@ public class UIManager : MonoBehaviour
         HideAllPanels();
         collectibleCanvas.SetActive(true);
         regionText.SetActive(true);
-        instructionTextMove.SetActive(true);
+        instructionTextMove.gameObject.SetActive(true);
+        StartCoroutine(FadeInMoveText());
     }
+    private IEnumerator FadeInMoveText()
+    {
+        isFading = true;
+        Color originalColor = instructionTextMove.color;
+        while (instructionTextMove.color.a < 1)
+        {
+            float newAlpha = instructionTextMove.color.a + Time.deltaTime;
+            instructionTextMove.color = new Color(originalColor.r, originalColor.g, originalColor.b, newAlpha);
+            yield return null;
+        }
+        isFading = false;
+        instructionTextMove.gameObject.SetActive(true);
+        shouldMove = true;
+    }
+
+    private IEnumerator FadeOutMoveText()
+    {
+        isFading = true;
+        Color originalColor = instructionTextMove.color;
+        while (instructionTextMove.color.a > 0)
+        {
+            float newAlpha = instructionTextMove.color.a - Time.deltaTime;
+            instructionTextMove.color = new Color(originalColor.r, originalColor.g, originalColor.b, newAlpha);
+            yield return null;
+        }
+        isFading= false;
+        instructionTextMove.gameObject.SetActive(false);
+        instructionTextJump.gameObject.SetActive(true);
+        StartCoroutine(FadeInJumpText());
+    }
+
+    private IEnumerator FadeInJumpText()
+    {
+        Debug.Log("jump");
+        isFading = true;
+        Color originalColor = instructionTextJump.color;
+        while (instructionTextJump.color.a < 1)
+        {
+            float newAlpha = instructionTextJump.color.a + Time.deltaTime;
+            instructionTextJump.color = new Color(originalColor.r, originalColor.g, originalColor.b, newAlpha);
+            yield return null;
+        }
+        isFading = false;
+        instructionTextJump.gameObject.SetActive(true);
+        shouldJump = true;
+    }
+
+    private IEnumerator FadeOutJumpText()
+    {
+        isFading = true;
+        Color originalColor = instructionTextJump.color;
+        while (instructionTextJump.color.a > 0)
+        {
+            float newAlpha = instructionTextJump.color.a - Time.deltaTime;
+            instructionTextJump.color = new Color(originalColor.r, originalColor.g, originalColor.b, newAlpha);
+            yield return null;
+        }
+        isFading = false;
+        instructionTextJump.gameObject.SetActive(false);
+    }
+
 
     public void ShowStartPanel()
     {
